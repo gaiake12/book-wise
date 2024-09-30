e5d3f4c0eeae34e137728f2401bce92abdd39fc5
gaiake12 <gaiake12@hotmail.com>
2024-09-30 09:29:33 -0300

    feat: feat adding profile picture

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,7 @@
 import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-import GitHubProvier from 'next-auth/providers/github'
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
+import GitHubProvier, { GithubProfile } from 'next-auth/providers/github'
 import { PrismaAdapter } from '@/lib/auth/prisma-adapter'
-
 
 export const authOptions = {
   adapter: PrismaAdapter(),
@@ -10,16 +9,29 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      profile(profile: GoogleProfile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          avatar_url: profile.picture,
+        }
+      },
     }),
 
     GitHubProvier({
       clientId: process.env.GITHUB_CLIENT_ID ?? '',
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
+      profile(profile: GithubProfile) {
+        return {
+          id: String(profile.id),
+          avatar_url: profile.avatar_url,
+          name: profile.name ?? '',
+          email: profile.email ?? '',
+        }
+      },
     }),
   ],
 }
 
-
 export default NextAuth(authOptions)
-
-
